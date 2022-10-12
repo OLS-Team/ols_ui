d3984ebea71d57cf6a5f488c468cdc57d864a8c3
sosensible <johnfarrar@sosapps.com>
2022-10-12 16:41:54 -0400

    Pinia integrated with counter example and put router links to main page.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container" style="padding: 50px 0 100px 0">
+    <RouterLink to="/">Home</RouterLink>&nbsp;|&nbsp;<RouterLink to="/about">About</RouterLink>
     <h3>Open Learning Server</h3>
     <router-view />
   </div>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,12 @@
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+
+const counter = useCounterStore();
+console.log(counter)
+</script>
+
 <template>
   My About View
+  <hr>
+  <button @click="counter.increment()">{{ counter.count }}</button>
 </template>
