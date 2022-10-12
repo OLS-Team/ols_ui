7c6f825a30dee471dd252dfe88d7ab7c9097f177
sosensible <johnfarrar@sosapps.com>
2022-10-12 16:16:04 -0400

    Pinia loaded but no code, Routers working

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,9 @@
 <template>
   <div class="container" style="padding: 50px 0 100px 0">
-    <h3>Working?</h3>
-    <Profile v-if="store.user" />
-    <Auth v-else />
+    <h3>Open Learning Server</h3>
+    <router-view />
   </div>
 </template>
 
 <script setup lang="ts">
-import { store } from "./store"
-import { supabase } from "./supabase"
-import Auth from "./components/Auth.vue"
-import Profile from "./components/Profile.vue"
-
-store.user = supabase.auth.user()
-supabase.auth.onAuthStateChange((_, session) => {
-  store.user = session.user
-})
 </script>
-<!-- <script>
-import { store } from "./store"
-import { supabase } from "./supabase"
-import Auth from "./components/Auth.vue"
-import Profile from "./components/Profile.vue"
-export default {
-  components: {
-    Auth,
-    Profile,
-  },
-
-  setup() {
-    store.user = supabase.auth.user()
-    supabase.auth.onAuthStateChange((_, session) => {
-      store.user = session.user
-    })
-
-    return {
-      store,
-    }
-  },
-}
-</script> -->
