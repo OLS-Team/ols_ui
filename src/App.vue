eb0f28f27d16c08e5627ce9f2f2b0158ba99a278
sosensible <johnfarrar@sosapps.com>
2022-10-12 15:54:28 -0400

    Login Email Working

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="container" style="padding: 50px 0 100px 0">
+    <h3>Working?</h3>
     <Profile v-if="store.user" />
     <Auth v-else />
   </div>
 </template>
 
-<script>
+<script setup lang="ts">
+import { store } from "./store"
+import { supabase } from "./supabase"
+import Auth from "./components/Auth.vue"
+import Profile from "./components/Profile.vue"
+
+store.user = supabase.auth.user()
+supabase.auth.onAuthStateChange((_, session) => {
+  store.user = session.user
+})
+</script>
+<!-- <script>
 import { store } from "./store"
 import { supabase } from "./supabase"
 import Auth from "./components/Auth.vue"
@@ -27,4 +39,4 @@ export default {
     }
   },
 }
-</script>
+</script> -->
